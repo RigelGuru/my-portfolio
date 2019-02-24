2253f9ba90567064edb2522a7104ac3eaaf66f34
Pirooz Faraj Zadeh <myrigelcode@gmail.com>
2019-02-25 07:37:39 +1100

    add upload myrigel script

---
+++ upload-myrigel-lambda.py
@@ -0,0 +1,14 @@
+import boto3
+import zipfile
+import mimetypes
+s3=boto3.resource('s3')
+myrigel_bucket=s3.Bucket('myrigel.com.au')
+build_bucket=s3.Bucket('codebuildmyrigel')
+import StringIO
+SIO_Portfolio_Zip=StringIO.StringIO()
+build_bucket.download_fileobj('myrigelbuild.zip',SIO_Portfolio_Zip)
+
+with zipfile.ZipFile(SIO_Portfolio_Zip) as myzip:
+    for nm in myzip.namelist():
+        obj= myzip.open(nm)
+        myrigel_bucket.upload_fileobj(obj,'portfolio/{}'.format(nm),ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
