c5a2734e81f6b6575902977e3874e19d7eae1bc2
Pirooz Faraj Zadeh <myrigelcode@gmail.com>
2019-02-26 05:14:20 +1100

    updated file from lambda codes

--- upload-myrigel-lambda.py
+++ upload-myrigel-lambda.py
@@ -1,14 +1,19 @@
 import boto3
 import zipfile
 import mimetypes
-s3=boto3.resource('s3')
-myrigel_bucket=s3.Bucket('myrigel.com.au')
-build_bucket=s3.Bucket('codebuildmyrigel')
-import StringIO
-SIO_Portfolio_Zip=StringIO.StringIO()
-build_bucket.download_fileobj('myrigelbuild.zip',SIO_Portfolio_Zip)
 
-with zipfile.ZipFile(SIO_Portfolio_Zip) as myzip:
-    for nm in myzip.namelist():
-        obj= myzip.open(nm)
-        myrigel_bucket.upload_fileobj(obj,'portfolio/{}'.format(nm),ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+def lambda_handler(event, context):
+
+    s3=boto3.resource('s3')
+    myrigel_bucket=s3.Bucket('myrigel.com.au')
+    build_bucket=s3.Bucket('codebuildmyrigel')
+    import StringIO
+    SIO_Portfolio_Zip=StringIO.StringIO()
+    build_bucket.download_fileobj('myrigelbuild.zip',SIO_Portfolio_Zip)
+
+    with zipfile.ZipFile(SIO_Portfolio_Zip) as myzip:
+        for nm in myzip.namelist():
+            obj= myzip.open(nm)
+            myrigel_bucket.upload_fileobj(obj,'portfolio/{}'.format(nm),ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+
+    return 'script sucessfully complited and published to demo'
